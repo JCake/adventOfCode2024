ab7c2455b8cc366198ae1f4d4b2665268354b4dc
Jessica C <jcake2@gmail.com>
2024-12-07 16:38:57 -0600

    Day 2

--- src/app/day2.pipe.spec.ts
+++ src/app/day2.pipe.spec.ts
@@ -4,9 +4,66 @@ describe('Day2Pipe', () => {
   const pipe = new Day2Pipe();
 
   describe('part1', () => {
-    it('should count as safe if all decreasing by 1 or 2', () => {
-      expect(pipe.transform('7 6 4 2 1').part1).toEqual('1');
+    it('should count as safe if all decreasing by 1, 2, or 3', () => {
+      expect(pipe.transform('8 7 4 2 1').part1).toEqual('1');
+    });
+
+    it('should count as unsafe if any number repeat', () => {
+      expect(pipe.transform('8 6 4 4 1').part1).toEqual('0');
+    });
+
+    it('should count as unsafe if any number decreases by more than 3', () => {
+      expect(pipe.transform('18 7 4 2 1').part1).toEqual('0');
+    });
+
+    it('should count as safe if all increasing by 1, 2, or 3', () => {
+      expect(pipe.transform('1 3 6 7 9').part1).toEqual('1');
+    });
+
+    it('should count as unsafe if numbers are a mix of increasing and decreasing', () => {
+      expect(pipe.transform('1 3 2 4 5').part1).toEqual('0');
+    });
+
+    it('should count as unsafe if numbers are a mix of increasing and decreasing - different order', () => {
+      expect(pipe.transform('5 3 6 4 1').part1).toEqual('0');
+    });
+
+    it('should handle extra whitespace', () => {
+      expect(pipe.transform(' 1  3  6  7   9 ').part1).toEqual('1');
+    });
+
+    it('should add up all safe from multiple lines of input', () => {
+      expect(pipe.transform(`7 6 4 2 1
+      1 2 7 8 9
+      9 7 6 2 1
+      1 3 2 4 5
+      8 6 4 4 1
+      1 3 6 7 9`).part1).toEqual('2');
+    });
+
+    
+    it('should work with multi-digit numbers', () => {
+      expect(pipe.transform(' 15  18  21  22   25 ').part1).toEqual('1');
     });
   });
+
+  describe('part 2', () => {
+    it('should now count as safe if a number being removed would make it safe', () => {
+      expect(pipe.transform('1 3 2 4 5').part2).toEqual('1');
+    });
+
+    it('should still count as unsafe if no number removal would make it safe', () => {
+      expect(pipe.transform('9 7 6 2 1').part2).toEqual('0');
+    });
+
+    it('should add up total number nearly safe', () => {
+      expect(pipe.transform(`7 6 4 2 1
+      1 2 7 8 9
+      9 7 6 2 1
+      1 3 2 4 5
+      8 6 4 4 1
+      1 3 6 7 9`).part2).toEqual('4');
+    })
+  })
   
 });
--- src/app/day2.pipe.ts
+++ src/app/day2.pipe.ts
@@ -8,7 +8,38 @@ import { Solution } from '../solutions';
 export class Day2Pipe implements PipeTransform {
 
   transform(input: string): Solution {
-    return {part1: '0', part2: '0'};
+    const rows = input.split('\n');
+    const parsedRows = rows.map(rowStr => rowStr.trim().split(/\s+/).map(num => parseInt(num)));
+    const safeRows = parsedRows.filter(this.safe);
+    const nearlySafeRows = parsedRows.filter(row => this.nearlySafe(row));
+    return {part1: `${safeRows.length}`, part2: `${nearlySafeRows.length}`};
+  }
+
+  safe(row: number[]): boolean {
+    const increasing = row[0] > row[1];
+    for(let i = 1; i < row.length; i++){
+      if(Math.abs(row[i] - row[i-1]) > 3){
+        return false;
+      }
+      if(increasing && row[i] >= row[i-1]){
+        return false;
+      }
+      if(!increasing && row[i] <= row[i-1]){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  nearlySafe(row: number[]): boolean {
+    for(let i = 0; i < row.length; i++){
+      const copy = row.slice();
+      copy.splice(i, 1);
+      if(this.safe(copy)){
+        return true;
+      }
+    }
+    return this.safe(row);
   }
 
 }
