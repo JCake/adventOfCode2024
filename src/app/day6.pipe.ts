04fbbf510d32b3b415a4bec3a9a12efbaf947287
Jessica C <jcake2@gmail.com>
2024-12-15 20:46:00 -0600

    Day 6 part 2

--- src/app/day6.pipe.ts
+++ src/app/day6.pipe.ts
@@ -9,14 +9,48 @@ export class Day6Pipe implements PipeTransform {
 
   transform(input: string): Solution {
     const grid = input.trim().split('\n').map(line => line.trim().split(''));
-    let x = 0;
-    let y = 0;
+    let openSpots = [];
     for(let yi = 0; yi < grid.length; yi++){
       for(let xi = 0; xi < grid[yi].length; xi++){
-        if(grid[yi][xi] === '^'){
+        if(grid[yi][xi] === '.'){
+          openSpots.push([yi,xi]);
+        }
+      }
+    }
+
+    this.travel(grid);
+    let traveledSpots = 0;
+    for (let yi = 0; yi < grid.length; yi++) {
+      for (let xi = 0; xi < grid[yi].length; xi++) {
+        if (grid[yi][xi] === 'X') {
+          traveledSpots++;
+        }
+      }
+    }
+
+    let loopCount = 0;
+    openSpots.forEach((spot) => {
+      const alteredGrid = input.trim().split('\n').map(line => line.trim().split(''));
+      alteredGrid[spot[0]][spot[1]] = '#';
+      if(this.travel(alteredGrid)) {
+        loopCount++;
+      }
+    })
+    return {part1: `${traveledSpots}`, part2: `${loopCount}`};
+  }
+
+  // returns true if the path loops
+  private travel(grid: string[][]): boolean {
+    let x = 0;
+    let y = 0;
+    let visited = new Set();
+    for (let yi = 0; yi < grid.length; yi++) {
+      for (let xi = 0; xi < grid[yi].length; xi++) {
+        if (grid[yi][xi] === '^') {
           x = xi;
           y = yi;
           grid[yi][xi] = 'X';
+          visited.add(`${x},${y}:^`);
           break;
         }
       }
@@ -24,10 +58,10 @@ export class Day6Pipe implements PipeTransform {
     // up:
     let xStep = 0;
     let yStep = -1;
-    while(x + xStep >= 0 && x + xStep < grid[y].length 
-      && y + yStep >= 0 && y + yStep < grid.length){
-      if(grid[y + yStep][x + xStep] === '#'){
-        if(xStep === 0){
+    while (x + xStep >= 0 && x + xStep < grid[y].length
+      && y + yStep >= 0 && y + yStep < grid.length) {
+      if (grid[y + yStep][x + xStep] === '#') {
+        if (xStep === 0) {
           xStep = xStep - yStep;
           yStep = 0;
         } else {
@@ -39,17 +73,25 @@ export class Day6Pipe implements PipeTransform {
         y = y + yStep;
         grid[y][x] = 'X';
       }
-    }
-
-    let traveledSpots = 0;
-    for(let yi = 0; yi < grid.length; yi++){
-      for(let xi = 0; xi < grid[yi].length; xi++){
-        if(grid[yi][xi] === 'X'){
-          traveledSpots++;
-        }
+      const spot = `${x},${y}:${this.direction(xStep, yStep)}`;
+      if(visited.has(spot)){
+        return true;
       }
+      visited.add(`${x},${y}:${this.direction(xStep, yStep)}`);
     }
-    return {part1: `${traveledSpots}`, part2: ''};
+    return false;
+  }
+
+  direction(xStep: number, yStep: number): string {
+    if(xStep === 0 && yStep === -1){
+      return '^'
+    } else if(xStep === 0 && yStep === 1){
+      return 'v';
+    } else if(xStep === 1){
+      return '>';
+    } else {
+      return '<';
+    }
   }
 
 }
