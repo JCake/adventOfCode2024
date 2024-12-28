f86597db327bf3b5afb9fbf5ff17aaf156a6b7c5
Jessica C <jcake2@gmail.com>
2024-12-28 13:57:01 -0600

    More Day 11, Part 2 experiments

--- src/app/day11.pipe.ts
+++ src/app/day11.pipe.ts
@@ -10,44 +10,70 @@ export class Day11Pipe implements PipeTransform {
   transform(input: string): Solution {
     let listOfStones: number[] = input.split(' ').map((s) => Number(s));
     let startToAfter: Map<number, number[]> = new Map();
-    listOfStones = this.buildNewList(listOfStones);
-    for(let i = 0; i < 4; i++){
-      let newList: number[] = [];
-      listOfStones.forEach((stone) => {
-        let after = startToAfter.get(stone);
-        if(after){
-          newList = newList.concat(after);
-        } else {
-          const additionalItems = this.expand(stone, 6);
-          startToAfter.set(stone, additionalItems);
-          newList = newList.concat(additionalItems);
-        }
-      })
-      listOfStones = newList;     
+    const startToStepsToOutput: Map<number, Map<number, number[]>> = new Map();
+    startToStepsToOutput.set(1, new Map());
+    let sampleList = [1];
+    for(let i = 0; i < 25; i++){
+      startToStepsToOutput.get(1)?.set(i, sampleList);
+      sampleList = this.buildNewList(sampleList);
     }
+    startToStepsToOutput.get(1)?.set(25, sampleList);
+
+    let finalList: number[] = [];
+    listOfStones.forEach((stone) => {
+      if(stone === 1){
+        finalList = finalList.concat(startToStepsToOutput.get(1)?.get(25) || []);
+      } else {
+        const newList = this.expand(stone, 25, startToStepsToOutput)
+        const steps = new Map<number, number[]>();
+        steps.set(25, newList)
+        startToStepsToOutput.set(stone, steps);
+        finalList = finalList.concat(newList);
+      }
+    })
+    listOfStones = finalList;
     const after25 = listOfStones.length;
 
     let after75 = 0;
     listOfStones.forEach((outerStone) => {
       listOfStones = [outerStone];
-      // Need 50 more iterations -> 6 x 8 = 48
-      for(let i = 0; i < 3; i++){ // TODO 3 needs to be 8
-        let newList: number[] = [];
-        listOfStones.forEach((stone) => {
-          let after = startToAfter.get(stone);
-          if(after){
-            newList = newList.concat(after);
-          } else {
-            const additionalItems = this.expand(stone, 6);
-            startToAfter.set(stone, additionalItems);
-            newList = newList.concat(additionalItems);
-          }
-        })
-        listOfStones = newList;  
-      }
+
       listOfStones.forEach((stone) => {
-        after75 += this.expand(stone, 2).length;
+        let stoneList: number[] = [];
+        if(startToStepsToOutput.get(stone)){
+          stoneList = stoneList.concat(startToStepsToOutput.get(stone)?.get(25) || []);
+        } else {
+          // TODO uncomment
+          // const newList = this.expand(stone, 25, startToStepsToOutput)
+          // const steps = new Map<number, number[]>();
+          // steps.set(25, newList)
+          // startToStepsToOutput.set(stone, steps);
+          // stoneList = stoneList.concat(newList);
+        }
+        // TODO another 25
+        after75 += stoneList.length;
       })
+      
+
+
+      // Need 50 more iterations -> 6 x 8 = 48
+      // for(let i = 0; i < 1; i++){ // TODO 3 needs to be 8
+      //   let newList: number[] = [];
+      //   listOfStones.forEach((stone) => {
+      //     let after = startToAfter.get(stone);
+      //     if(after){
+      //       newList = newList.concat(after);
+      //     } else {
+      //       const additionalItems = this.expand(stone, 6);
+      //       startToAfter.set(stone, additionalItems);
+      //       newList = newList.concat(additionalItems);
+      //     }
+      //   })
+      //   listOfStones = newList;  
+      // }
+      // listOfStones.forEach((stone) => {
+      //   after75 += this.expand(stone, 2).length;
+      // })
     });
 
     return {part1: `${after25}`, part2: `${after75}`};
@@ -57,12 +83,19 @@ export class Day11Pipe implements PipeTransform {
     return listsOfStones.map(l => l.length).reduce((prev,curr) => prev + curr, 0);
   }
 
-  private expand(stone: number, steps: number): number[] {
+  private expand(stone: number, steps: number, startToStepsToOutput: Map<number, Map<number, number[]>> = new Map()): number[] {
     let expanded = [stone];
+    let others: number[] = [];
     for(let i = 0; i < steps; i++){
+      if(startToStepsToOutput.has(1)){
+        expanded.filter((e) => e === 1).forEach((one) => {
+          others = others.concat(startToStepsToOutput.get(1)?.get(steps - i) || []);
+        });
+        expanded = expanded.filter((e) => e !== 1);
+      }
       expanded = this.buildNewList(expanded);
     }
-    return expanded;
+    return expanded.concat(others);
   }
 
   private buildNewList(stones: number[]) {
